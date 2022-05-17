9166127c4ee9effb605fdc74a4539b106b45d176
ibrahim kenia <ibrahimkenia2@gmail.com>
2022-05-17 11:15:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,93 @@
+// Import All Dependencies
+const dotenv = require('dotenv');
+const express =require('express');
+const bcryptjs=require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const app = express();
+// Configure ENV File & Require Connection File
+dotenv.config({path:'./config.env'});
+require('./db/conn');
+const port = process.env.PORT;
+// Require Model
+const Users = require('./models/userSchema');
+const Message = require('./models/msgSchema');
+
+// These Method is used to get Data and Coookies from frontEnd
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+})
+
+//Registration 
+app.post('/register',async(req,res)=>{
+    try{
+      // get the body or data
+      const username = req.body.username;
+      const email = req.body.email;
+      const password = req.body.password;
+      const createUser = new Users({
+          username:username,
+          email:email,
+          password:password,
+      });
+      // Save Method is Used to Create User or Insert User
+      // But Before Saving or Inserting, Password will Hash
+      // Because of hashing. After Hash, It will save to DB
+      const created = await createUser.save();
+      console.log(created);
+      res.status(200).send("Registred");
+    }catch (error){
+        res.status(400).send(error)
+    }
+
+})
+//Login User
+app.post('/login', async (req, res)=>{
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        // Find User if Exist
+        const user = await Users.findOne({email : email});
+        if(user){
+            // Verify Password
+            const isMatch = await bcryptjs.compare(password, user.password);
+
+            if(isMatch){
+                // Generate Token Which is Define in User Schema
+                const token = await user.generateToken();
+                res.cookie("jwt", token, {
+                    // Expires Token in 24 Hours
+                    expires : new Date(Date.now() + 86400000),
+                    httpOnly : true
+                })
+                res.status(200).send("LoggedIn")
+            }else{
+                res.status(400).send("Invalid Credentials");
+            }
+        }else{
+            res.status(400).send("Invalid Credentials");
+        }
+
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+//Logout Page
+app.get('/logout',(req,res)=>{
+   res.clearCookie("jwt",{pzth:'/'})
+   res.status(200).send("User Logged Out")
+})
+
+
+
+//Run Server
+app.listen(port,()=>{
+    console.log("Server is Listening")
+})
+// Our Backend is Done And Store And Store dATA IN database
+// Now Its Time to connect Front End with Backend
